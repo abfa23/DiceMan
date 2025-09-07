8d8d118bebad5e4f53db241a3e79969ea346cf3b
Frederick <frdu0001@stud@ek.dk>
2025-09-07 13:23:31 +0200

    test2

--- src/DiceMan/DiceManTest.java
+++ src/DiceMan/DiceManTest.java
@@ -1,4 +1,21 @@
 package DiceMan;
 
-public class DiceManTest {
+public class DiceManTest{
+    private Dice dice;
+    private DiceMan diceman;
+
+    public DiceManTest() {
+        dice = new Dice();
+        diceman = new DiceMan();
+    }
+    public void start() {
+        int roll = dice.numbers;
+        System.out.println("dit Roll: " + roll);
+        diceman.lavAktivitet(roll);
+    }
+
+    public static void main(String[] args) {
+        DiceManTest game = new DiceManTest();
+        game.start();
+    }
 }
